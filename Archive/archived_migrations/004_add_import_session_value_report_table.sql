21a47eef20fb2ae0008a6d0b5255f361462510ac
black77dragon <rene.w.keller@gmail.com>
2026-01-28 21:19:42 +0100

    clean_new_NG_version

--- Archive/archived_migrations/004_add_import_session_value_report_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS ImportSessionValueReports (
-    report_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    import_session_id INTEGER NOT NULL,
-    instrument_name TEXT NOT NULL,
-    currency TEXT NOT NULL,
-    value_orig REAL NOT NULL,
-    value_chf REAL NOT NULL,
-    FOREIGN KEY (import_session_id) REFERENCES ImportSessions(import_session_id)
-);
--- Archive/archived_migrations/005_add_target_kind_and_tolerance.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE TargetAllocation
-  ADD COLUMN target_kind TEXT NOT NULL DEFAULT 'percent' CHECK(target_kind IN('percent','amount'));
-
-ALTER TABLE TargetAllocation
-  ADD COLUMN tolerance_percent REAL NOT NULL DEFAULT 5.0;
